db1c88af48e34c4ff2e2106b579751923843f9df
Pranay Borode <pranayborode14@gmail.com>
2024-01-17 21:41:53 +0530

    Enum practice Code..

---
+++ PracticeCode/src/com/enumdemo/Test.java
@@ -0,0 +1,16 @@
+package com.enumdemo;
+
+enum Color {
+	RED,
+	GREEN,
+	BLUE;
+}
+
+public class Test {
+
+	public static void main(String[] args) {
+		Color c = Color.RED;
+		System.out.println(c);
+	}
+
+}
---
+++ PracticeCode/src/com/enumdemo/Test2.java
@@ -0,0 +1,16 @@
+package com.enumdemo;
+
+public class Test2 {
+
+	enum Color{
+		RED,
+		WHITE;
+	}
+	
+	public static void main(String[] args) {
+		Color c = Color.WHITE;
+		System.out.println(c);
+
+	}
+
+}
